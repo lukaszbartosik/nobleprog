236e9c8ccc944a19c99de724b1c5c9f944484304
Lukasz Bartosik <lukaszbartosik99@outlook.com>
2022-11-14 19:03:15 +0000

    Add rubocop

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,27 @@
-class StaticPagesController < ApplicationController
-    def privacy
-        if params[:lang] == 'pl'
-            render "privacy_pl"
-        elsif params[:lang] == 'es'
-            render "privacy_es"
-        else
-            @privacy_tekst = Faker::Lorem.paragraphs(number: 6)
-#            render "privacy"
-        end
-    end
+# frozen_string_literal: true
 
+class StaticPagesController < ApplicationController
+  def privacy
+    case params[:lang]
+    when 'pl'
+      render 'privacy_pl'
+    when 'es'
+      render 'privacy_es'
+    else
+      @privacy_tekst = Faker::Lorem.paragraphs(number: 6)
+      #            render "privacy"
+    end
+  end
+
+  def current_time; end
+
+  private
+
+  helper_method :daytime?
+
+  def daytime?
+    hour = Time.zone.now.hour
+
+    (hour > 6 && hour < 17)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get 'privacy', to: 'static_pages#privacy'
+
+  get 'aktualny_czas', to: 'static_pages#current_time'
 end
